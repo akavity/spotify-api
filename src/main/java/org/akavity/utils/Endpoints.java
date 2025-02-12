3e7cf317c3db76394808943d0f2b8bff3df93f01
Basil Akavity <vakavity@gmail.com>
2025-02-12 13:11:09 +0300

    added endpoints

--- src/main/java/org/akavity/utils/Endpoints.java
+++ src/main/java/org/akavity/utils/Endpoints.java
@@ -1,7 +1,7 @@
 package org.akavity.utils;
 
 public class Endpoints {
-    public static final String CLIENT_ID = "d9edbd1d682f4634b52688b96aa4af34";
-    public static final String CLIENT_SECRET = "5d1f3506dc1b4c16b455cd035784fff6";
+    public static final String CLIENT_ID = "client_id";        // client_id and client_secret can be found in the settings https://developer.spotify.com/dashboard
+    public static final String CLIENT_SECRET = "client_secret";
     public static final String TOKEN = "https://accounts.spotify.com/api/token";
 }
