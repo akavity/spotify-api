a69a4865e5846f3d10af547ad717a439c5ad60d8
Basil Akavity <vakavity@gmail.com>
2025-02-13 11:09:02 +0300

    added extractText to Utils

--- src/main/java/org/akavity/utils/Utils.java
+++ src/main/java/org/akavity/utils/Utils.java
@@ -1,15 +1,26 @@
 package org.akavity.utils;
 
+import io.restassured.path.json.JsonPath;
+import org.akavity.enums.PathEnum;
+
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Utils {
-    public String extractText(String text, String regex) {
+    public List<String> extractTracks(JsonPath jsonPath, PathEnum pathEnum) {
+        return jsonPath.getList(pathEnum.getPath(), String.class)
+                .stream()
+                .map(x -> extractText(x, "^[^(-]+"))
+                .toList();
+    }
+
+    private String extractText(String text, String regex) {
         String result = "";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
-            result = (matcher.group().replace("(", "").trim());
+            result = (matcher.group().trim());
         }
         return result;
     }
